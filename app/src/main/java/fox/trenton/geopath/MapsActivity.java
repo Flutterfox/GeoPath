dc9755ea33a0c63911432a73409578fa06810f83
flutterfox <tcfox54@gmail.com>
2016-02-22 21:18:02 -0600

    Maps activity now activates when the fabAdd is clicked.
    (Current errors from attempt to record current location)

--- app/src/main/java/fox/trenton/geopath/MapsActivity.java
+++ app/src/main/java/fox/trenton/geopath/MapsActivity.java
@@ -1,10 +1,16 @@
 package fox.trenton.geopath;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,23 +19,29 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private double latitude, longitude;
+    LocationManager locationManager;
+    List<Location> locList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
-                LOCATION_REFRESH_DISTANCE, mLocationListener);
     }
 
 
@@ -47,15 +59,97 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         //Move to current location
-        LatLng loc = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(loc).title("Current Location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        showCurrentLocation(getLocation());
+
+        //Collects location periodically
+        Timer t = new Timer(true);
+        t.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        addCurrentLocation(getLastBestLocation(locationManager));
+                    }
+                });
+
+            }
+
+        }, 0, 30000);
     }
 
-    private final LocationListener mLocationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(final Location location) {
-            //your code here
+    private void showCurrentLocation(Location location) {
+        LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(loc).title("Current Location"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 18));
+    }
+
+    private void addCurrentLocation(Location location) {
+        LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(loc).title("Previous Location"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 18));
+    }
+
+    private Location getLocation() {
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+
+        String provider = "";
+        if (locationManager != null) {
+            provider = locationManager.getBestProvider(criteria, true);
         }
-    };
+
+        LocationListener locationListener = new LocationListener() {
+
+            @Override
+            public void onLocationChanged(Location location) {
+                showCurrentLocation(location);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+            }
+        };
+
+        Location location = null;
+        if (locationManager != null) {
+            locationManager.requestLocationUpdates(provider, 2000, 0, locationListener);
+            location = getLastBestLocation(locationManager);
+        }
+        locList.add(location);
+        return location;
+    }
+
+
+    //Compares gps and network locations, chooses best, stores it in the list
+    private Location getLastBestLocation(LocationManager lm) {
+        Location locationGPS = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        Location locationNet = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        long GPSLocationTime = 0;
+        if (null != locationGPS) {
+            GPSLocationTime = locationGPS.getTime();
+        }
+
+        long NetLocationTime = 0;
+
+        if (null != locationNet) {
+            NetLocationTime = locationNet.getTime();
+        }
+
+        if (0 < GPSLocationTime - NetLocationTime) {
+            return locationGPS;
+        } else {
+            return locationNet;
+        }
+    }
+
 }
