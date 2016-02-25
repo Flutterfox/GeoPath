c3c93c3acc4881c9c1c5f4e22a8db2b3fae85d01
flutterfox <tcfox54@gmail.com>
2016-02-24 21:10:01 -0600

    Added gradle files to git. Database helper is finished (allows for creating db). Database connector is under construction (allows for CRUD). An app icon was added.

---
+++ app/src/main/java/fox/trenton/geopath/DatabaseConnector.java
@@ -0,0 +1,134 @@
+package fox.trenton.geopath;
+
+/**
+ * Created by trenton on 2/24/16.
+ */
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.location.Location;
+import android.provider.Settings;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DatabaseConnector {
+
+    // Declare Variables
+    private static final int DATABASE_VERSION = 1;
+
+    //Database Name
+    private static final String DB_NAME = "GeoPath";
+
+    //Table Names
+    private static final String TABLE_LOC = "LOCATION";
+    private static final String TABLE_PATH = "PATH";
+    private static final String TABLE_USERS = "USERS";
+
+    //Common Column names
+    private static final String USERID = "USERID";
+    private static final String PATHID = "PATHID";
+
+    //Path Table - Column names
+    private static final String PATHLABEL = "LABEL";
+    private static final String PATHDESCRIPTION = "DESCRIPTION";
+
+    //Location Table - Column names
+    private static final String LOCATIONID = "LOCATIONID";
+    private static final String LATITUDE = "LATITUDE";
+    private static final String LONGITUDE = "LONGITUDE";
+    private static final String TIMEDATE = "TIMEDATE";
+    private static final String TYPE = "TYPE";
+    private static final String LOCLABEL = "LABEL";
+    private static final String LOCDESCRIPTION = "DESCRIPTION";
+
+
+    private SQLiteDatabase database;
+    private DatabaseHelper dbOpenHelper;
+
+    public DatabaseConnector(Context context) {
+        dbOpenHelper = new DatabaseHelper(context, null,
+                DATABASE_VERSION);
+
+    }
+
+    // Open Database function
+    public void open() throws SQLException {
+        // Allow database to be in writable mode
+        database = dbOpenHelper.getWritableDatabase();
+    }
+
+    // Close Database function
+    public void close() {
+        if (database != null)
+            database.close();
+    }
+
+    // Create Database function
+    public void InsertLocation(Location location, String type, String label, String description, String deviceID, String pathid) {
+        String locationID = (location.getTime()) + deviceID;
+        Date timestamp = new Date(location.getTime());
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/YYYY", Locale.getDefault());
+        String timedate = sdf.format(timestamp);
+
+        ContentValues newCon = new ContentValues();
+        newCon.put(LOCATIONID, locationID);
+        newCon.put(LATITUDE, location.getLatitude());
+        newCon.put(LONGITUDE, location.getLongitude());
+        newCon.put(USERID, deviceID);
+        newCon.put(TIMEDATE, timedate);
+        newCon.put(TYPE, type);
+        newCon.put(LOCLABEL, label);
+        newCon.put(LOCDESCRIPTION, description);
+        newCon.put(PATHID, pathid);
+
+        open();
+        database.insert(TABLE_LOC, null, newCon);
+        close();
+    }
+
+    // Update Database function
+    public void UpdateLocation(String locID, Location location, String type, String label, String description, String deviceID, String pathid) {
+        Date timestamp = new Date(location.getTime());
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/YYYY", Locale.getDefault());
+        String timedate = sdf.format(timestamp);
+
+        ContentValues editCon = new ContentValues();
+        editCon.put(LOCATIONID, locID);
+        editCon.put(LATITUDE, location.getLatitude());
+        editCon.put(LONGITUDE, location.getLongitude());
+        editCon.put(USERID, deviceID);
+        editCon.put(TIMEDATE, timedate);
+        editCon.put(TYPE, type);
+        editCon.put(LOCLABEL, label);
+        editCon.put(LOCDESCRIPTION, description);
+        editCon.put(PATHID, pathid);
+
+        open();
+        database.update(TABLE_LOC, editCon, LOCATIONID + "=" + locID, null);
+        close();
+    }
+
+    // Delete Database function
+    public void DeleteLocation(long locID) {
+        open();
+        database.delete(TABLE_LOC, LOCATIONID + "=" + locID, null);
+        close();
+    }
+
+    // List all data function
+    public Cursor ListAllLocations() {
+        return database.query(TABLE_LOC, new String[] { LOCATIONID}, null,
+                null, null, null, TITLE);
+    }
+
+    // Capture single data by ID
+    public Cursor GetOneLocation(long locID) {
+        return database.query(TABLE_LOC, null, LOCATIONID + "=" + locID, null, null,
+                null, null);
+    }
+
+}
---
+++ app/src/main/java/fox/trenton/geopath/DatabaseHelper.java
@@ -0,0 +1,89 @@
+package fox.trenton.geopath;
+
+/**
+ * Created by trenton on 2/24/16.
+ */
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    // Declare Variables
+    private static final int DATABASE_VERSION = 1;
+
+    //Database Name
+    private static final String DB_NAME = "GeoPath";
+
+    //Table Names
+    private static final String TABLE_LOC = "LOCATION";
+    private static final String TABLE_PATH = "PATH";
+    private static final String TABLE_USERS = "USERS";
+
+    //Common Column names
+    private static final String USERID = "USERID";
+    private static final String PATHID = "PATHID";
+
+    //Path Table - Column names
+    private static final String PATHLABEL = "LABEL";
+    private static final String PATHDESCRIPTION = "DESCRIPTION";
+
+    //Location Table - Column names
+    private static final String LOCATIONID = "LOCATIONID";
+    private static final String LATITUDE = "LATITUDE";
+    private static final String LONGITUDE = "LONGITUDE";
+    private static final String TIMEDATE = "TIMEDATE";
+    private static final String TYPE = "TYPE";
+    private static final String LOCLABEL = "LABEL";
+    private static final String LOCDESCRIPTION = "DESCRIPTION";
+
+    public DatabaseHelper(Context context, CursorFactory factory, int version) {
+        super(context, DB_NAME, factory, version);
+    }
+
+    //Table Create Statements
+    //Create Table Users
+    private static final String CREATE_TABLE_USERS = "CREATE TABLE "
+            + TABLE_USERS + "(" + USERID + " TEXT PRIMARY KEY)";
+
+    //Create Table Path
+    private static final String CREATE_TABLE_PATH = "CREATE TABLE " + TABLE_PATH + "("
+            + PATHID + " TEXT PRIMARY KEY, "
+            + USERID + " TEXT,"
+            + PATHLABEL + " TEXT,"
+            + PATHDESCRIPTION + " TEXT)";
+
+    //Create Table Location
+    private static final String CREATE_TABLE_LOCATION = "CREATE TABLE " + TABLE_LOC + "("
+            + LOCATIONID + " TEXT PRIMARY KEY, "
+            + LATITUDE + " INTEGER, "
+            + LONGITUDE + " INTEGER, "
+            + USERID + " TEXT, "
+            + TIMEDATE + " DATETIME, "
+            + TYPE + " TEXT, "
+            + LOCLABEL + " TEXT, "
+            + LOCDESCRIPTION + " TEXT, "
+            + PATHID + " TEXT)";
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Creating required tables
+        db.execSQL(CREATE_TABLE_USERS);
+        db.execSQL(CREATE_TABLE_PATH);
+        db.execSQL(CREATE_TABLE_LOCATION);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Database will be wiped on version change
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_LOC);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PATH);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
+
+        //Create new tables
+        onCreate(db);
+    }
+
+}
