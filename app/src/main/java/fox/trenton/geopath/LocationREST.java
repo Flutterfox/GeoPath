68e9f44332ac7170f7d1033cc4e39d3075202791
flutterfox <tcfox54@gmail.com>
2016-03-07 23:32:01 -0600

    Initial support for communication with the tomcat service

---
+++ app/src/main/java/fox/trenton/geopath/LocationREST.java
@@ -0,0 +1,39 @@
+package fox.trenton.geopath;
+
+import android.widget.Toast;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
+/**
+ * Created by trenton on 3/7/16.
+ */
+public class LocationREST {
+    public static final String JSON_URL = "http://172.25.6.62:8080/GeoPathServer/rest/location";
+
+    private void sendRequest(){
+        StringRequest stringRequest = new StringRequest(JSON_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        showJSON(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //Toast.makeText(MainActivity.this,error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private void showJSON(String json){
+        ParseJSON pj = new ParseJSON(json);
+        pj.parseJSON();
+        CustomList cl = new CustomList(this, ParseJSON.ids,ParseJSON.names,ParseJSON.emails);
+        listView.setAdapter(cl);
+    }
+}
