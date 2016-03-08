0b44de7b7679dff20a0c6a00b707ff98cf503deb
flutterfox <tcfox54@gmail.com>
2016-03-07 23:20:09 -0600

    Initial support for communication with the tomcat service

--- app/src/main/java/fox/trenton/geopath/DatabaseConnector.java
+++ app/src/main/java/fox/trenton/geopath/DatabaseConnector.java
@@ -10,7 +10,6 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.location.Location;
-import android.provider.Settings;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
